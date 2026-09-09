761699c1108994a0fde6dfab8eaa16c16a686962
kindsmell <kindsmell33@gmail.com>
2026-09-09 12:40:55 +0900

    ✨ feat: S03 오각형 메쉬 스크립트 스켈레톤 추가

---
+++ Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh_Pentagon : MonoBehaviour
+{
+    void Start()
+    {
+        // 정오각형(정점 5개) 메시를 생성한다.
+    }
+}
