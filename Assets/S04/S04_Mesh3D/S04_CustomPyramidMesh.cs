688c86f486f441380e03dbb5a3e229f3c20fba41
kindsmell <kindsmell33@gmail.com>
2026-09-10 10:37:59 +0900

    ✨ feat: S04 과제 폴더 추가 (다이아몬드 스크립트는 과제 최상위에 배치)

---
+++ Assets/S04/S04_Mesh3D/S04_CustomPyramidMesh.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S04_CustomPyramidMesh : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),     // 0
+            new Vector3(1f, 0f, 0f),     // 1
+            new Vector3(1f, 0f, 1f),     // 2
+            new Vector3(0f, 0f, 1f),     // 3
+            new Vector3(0.5f, 1f, 0.5f), // 4 (꼭짓점)
+        };
+
+        int[] triangles = new int[]
+        {
+            // 밑면 (아래를 향하도록)
+            0, 2, 3,
+            0, 1, 2,
+
+            // 옆면 앞 (0,1,4)
+            0, 4, 1,
+            // 옆면 오른쪽 (1,2,4)
+            1, 4, 2,
+            // 옆면 뒤 (2,3,4)
+            2, 4, 3,
+            // 옆면 왼쪽 (3,0,4)
+            3, 4, 0,
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
